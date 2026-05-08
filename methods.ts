56ab1c49505a9ae371458637e4e08b3063668f53
Leask Wong <i@leaskh.com>
2026-05-08 09:36:28 -0400

    fix: tighten editMessageText types

--- methods.ts
+++ methods.ts
@@ -1900,9 +1900,9 @@ export type ApiMethods<F> = {
     /** Unique identifier of the business connection on behalf of which the message to be edited was sent */
     business_connection_id?: string;
     /** Required if inline_message_id is not specified. Unique identifier for the target chat or username of the target channel (in the format `@channelusername`) */
-    chat_id?: number | string;
+    chat_id: number | string;
     /** Required if inline_message_id is not specified. Identifier of the message to edit */
-    message_id?: number;
+    message_id: number;
     /** Required if chat_id and message_id are not specified. Identifier of the inline message */
     inline_message_id?: undefined;
     /** New text of the message, 1-4096 characters after entities parsing */
@@ -1915,7 +1915,10 @@ export type ApiMethods<F> = {
     link_preview_options?: LinkPreviewOptions;
     /** An object for an inline keyboard. */
     reply_markup?: InlineKeyboardMarkup;
-  }): Update.Edited & Message.TextMessage & Message.BusinessSentMessage;
+  }):
+    & Update.Edited
+    & (Message.TextMessage | Message.GameMessage)
+    & Message.BusinessSentMessage;
 
   /** Use this method to edit inline text and game messages. On success, True is returned. Note that business messages that were not sent by the bot and do not contain an inline keyboard can only be edited within 48 hours from the time they were sent. */
   editMessageText(args: {
@@ -1926,7 +1929,7 @@ export type ApiMethods<F> = {
     /** Required if inline_message_id is not specified. Identifier of the message to edit */
     message_id?: undefined;
     /** Required if chat_id and message_id are not specified. Identifier of the inline message */
-    inline_message_id?: string;
+    inline_message_id: string;
     /** New text of the message, 1-4096 characters after entities parsing */
     text: string;
     /** Mode for parsing entities in the message text. See formatting options for more details. */
